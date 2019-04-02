f61c041b0fbd389f6bcf54f191dccf0addfd6f30
Patryk <patryk.xperia.s@gmail.com>
2019-04-02 14:46:03 +0200

    Prototyp lobby

--- HEX/Assets/Scripts/PlayerScript.cs
+++ HEX/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,13 @@ public class PlayerScript : NetworkBehaviour
 {
     [SyncVar]
     public bool myTurn = false;
+    [SyncVar]
+    public bool lobby = true;
+    [SyncVar]
+    public bool ready = false;
+
+    public int buttonSizeW = 200;
+    public int buttonSizeH = 200;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +23,7 @@ public class PlayerScript : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public override void OnStartLocalPlayer()
@@ -30,14 +37,31 @@ public class PlayerScript : NetworkBehaviour
         ServManager.Instance.AlterTurns();
     }
 
+    [Command]
+    void CmdReady()
+    {
+        ready = !ready;
+    }
+
     void OnGUI()
     {
         if (isLocalPlayer)
         {
-            GUI.Label(new Rect(10, 10, 100, 100), myTurn.ToString() + playerControllerId);
-            if (GUI.Button(new Rect(10, 10, 100, 100), "Test"))
+            if (lobby)
             {
-                CmdAlterTurn();
+                GUI.Label(new Rect(Screen.width / 2 - buttonSizeW / 2, Screen.height / 2 - buttonSizeH / 2, buttonSizeW, buttonSizeH), ready ? "Gotowy" : "Nie gotowy");
+
+                if (GUI.Button(new Rect(10, 10, 100, 100), "Gotowość"))
+                {
+                    CmdReady();
+                }
+            }
+            else
+            {
+                if (GUI.Button(new Rect(10, 10, 100, 100), "Koniec tury"))
+                {
+                    CmdAlterTurn();
+                }
             }
         }
     }
--- HEX/Assets/Scripts/ServManager.cs
+++ HEX/Assets/Scripts/ServManager.cs
@@ -5,9 +5,11 @@ using UnityEngine.Networking;
 
 public class ServManager : NetworkManager
 {
+    public int maxPlayers = 4;
 
     List<NetworkConnection> players;
     int activePlayer = 0;
+    bool lobby = true;
 
     public static ServManager Instance;
     // Start is called before the first frame update
@@ -27,13 +29,41 @@ public class ServManager : NetworkManager
     // Update is called once per frame
     void Update()
     {
-        
+        if (lobby)
+        {
+            if (players.Count > 1)
+            {
+                bool ready = true;
+                foreach (var player in players)
+                {
+                    if (!player.playerControllers[0].gameObject.GetComponent<PlayerScript>().ready)
+                    {
+                        ready = false;
+                        break;
+                    }
+                }
+                if (ready && players.Count > 1)
+                {
+                    StartGame();
+                }
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        lobby = false;
+        foreach (var player in players)
+        {
+            player.playerControllers[0].gameObject.GetComponent<PlayerScript>().lobby = false;
+        }
+        Debug.Log("game started");
     }
 
     public void AlterTurns()
     {
         Debug.Log("Zmiana gracza");
-        foreach(var player in players)
+        foreach (var player in players)
         {
             player.playerControllers[0].gameObject.GetComponent<PlayerScript>().myTurn = false;
         }
@@ -45,20 +75,22 @@ public class ServManager : NetworkManager
 
     public override void OnServerConnect(NetworkConnection conn)
     {
-        Debug.Log(conn);
-        players.Add(conn);
-        Debug.Log(conn.playerControllers.Count);
-    }
-
-    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-    {
-        base.OnServerAddPlayer(conn, playerControllerId);
-        Debug.Log("added");
+        if (lobby && players.Count < maxPlayers)
+        {
+            Debug.Log(conn);
+            players.Add(conn);
+        }
+        else
+        {
+            conn.Disconnect();
+        }
     }
 
     public override void OnServerDisconnect(NetworkConnection conn)
     {
-        players.Remove(conn);
+        if (lobby)
+        {
+            players.Remove(conn);
+        }
     }
-
 }
