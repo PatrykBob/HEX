7301d4e1fbbe2e905d14a62c11973d6ddf7d35fa
PatrykBob <patryk.xperia.s@gmail.com>
2019-05-12 23:48:08 +0200

    Added token rotation to InputManager and added necessary elements to token prefab

--- HEX/Assets/Scripts/InputManagerScript.cs
+++ HEX/Assets/Scripts/InputManagerScript.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject SelectedToken;
 
     public bool moving;
-    // Update is called once per frame
+    public bool rotating;
     void Update()
     {
         if(Input.touchCount > 0)
@@ -19,14 +19,31 @@ public class InputManagerScript : MonoBehaviour
 
                 RaycastHit[] hits = Physics.RaycastAll(ray);
 
+                
+            
                 if(Input.GetTouch(0).phase == TouchPhase.Began)
                 {
+                    bool rotatingHit = false;
+                    bool movingHit = false;
+
                     foreach (var hit in hits)
                     {
-                        if (hit.transform.gameObject == SelectedToken)
+                        if (hit.transform.gameObject.name == "RotationQuad")
                         {
-                            moving = true;
+                            rotatingHit = true;
                         }
+                        if (hit.transform.gameObject.name == "MovingQuad")
+                        {
+                            movingHit = true;
+                        }
+                    }
+                    if (movingHit)
+                    {
+                        moving = true;
+                    }
+                    else if (rotatingHit && !movingHit)
+                    {
+                        rotating = true;
                     }
                 }
 
@@ -40,6 +57,19 @@ public class InputManagerScript : MonoBehaviour
                         }
                     }
                 }
+
+                if (rotating)
+                {
+                    foreach (var hit in hits)
+                    {
+                        if (hit.transform.name == "TokenRotationQuad")
+                        {
+                            SelectedToken.transform.rotation = Quaternion.LookRotation(hit.point) * Quaternion.Euler(-90, -90,0);
+                        }
+                    }
+
+                }
+
                 if(Input.GetTouch(0).phase == TouchPhase.Ended)
                 {
                     if (moving)
@@ -51,10 +81,104 @@ public class InputManagerScript : MonoBehaviour
                                 SelectedToken.transform.position = hit.transform.position;
                             }
                         }
+                        moving = false;
                     }
-                    moving = false;
+                    else if (rotating)
+                    {
+                        Vector3 rot = new Vector3(270, 0, 0);
+                        float rotation = SelectedToken.transform.eulerAngles.y;
+                        if (rotation >= 240 && rotation < 300)
+                        {
+                            rot.y = 270;
+                            SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+                        }
+                        else if (rotation >= 300 && rotation < 360)
+                        {
+                            rot.y = 330;
+                            SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+                        }
+                        else if (rotation >= 0 && rotation < 60)
+                        {
+                            rot.y = 30;
+                            SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+                        }
+                        else if (rotation >= 60 && rotation < 120)
+                        {
+                            rot.y = 90;
+                            SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+                        }
+                        else if (rotation >= 120 && rotation < 180)
+                        {
+                            rot.y = 160;
+                            SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+                        }
+                        else if (rotation >= 180 && rotation < 240)
+                        {
+                            rot.y = 210;
+                            SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+                        }
+                        rotating = false;
+                    }
                 }
             }
-        }   
+        }
+        //For testing in PC
+        //if (Input.GetMouseButton(0))
+        //{
+        //    if (!rotating)
+        //    {
+        //        Debug.Log(SelectedToken.transform.localEulerAngles);
+        //    }
+        //    rotating = true;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit[] hits = Physics.RaycastAll(ray);
+        //    if (rotating)
+        //    {
+        //        foreach (var hit in hits)
+        //        {
+        //            if (hit.transform.name == "TokenRotationQuad")
+        //            {
+        //                SelectedToken.transform.rotation = Quaternion.LookRotation(hit.point) * Quaternion.Euler(-90, -90, 0);
+        //            }
+        //        }
+
+        //    }
+        //}
+        //else
+        //{
+        //    float rotation = SelectedToken.transform.eulerAngles.y;
+        //    Vector3 rot = new Vector3(270, 0, 0);
+        //    if(rotation >= 240 && rotation < 300)
+        //    {
+        //        rot.y = 270;
+        //        SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+        //    }
+        //    else if(rotation >= 300 && rotation < 360)
+        //    {
+        //        rot.y = 330;
+        //        SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+        //    }
+        //    else if (rotation >= 0 && rotation < 60)
+        //    {
+        //        rot.y = 30;
+        //        SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+        //    }
+        //    else if (rotation >= 60 && rotation < 120)
+        //    {
+        //        rot.y = 90;
+        //        SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+        //    }
+        //    else if (rotation >= 120 && rotation < 180)
+        //    {
+        //        rot.y = 160;
+        //        SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+        //    }
+        //    else if (rotation >= 180 && rotation < 240)
+        //    {
+        //        rot.y = 210;
+        //        SelectedToken.transform.localRotation = Quaternion.Euler(rot);
+        //    }
+        //    rotating = false;
+        //}
     }
 }
