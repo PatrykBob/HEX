d56a92bc7cef1c866dd5bc71b0fe6545f2c2eb4c
PatrykBob <patryk.xperia.s@gmail.com>
2019-05-21 15:18:18 +0200

    Added spawning tokens using token scriptable object

--- HEX/Assets/Scripts/TokenScript.cs
+++ HEX/Assets/Scripts/TokenScript.cs
@@ -6,10 +6,31 @@ public class TokenScript : MonoBehaviour
 {
     public int i;
     public int j;
+
+    public TokenScriptableObject tokenObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (tokenObject.initiation > 0)
+        {
+            GameObject initiationText = Instantiate(Resources.Load<GameObject>("Prefabs/InitiationText"));
+            initiationText.transform.SetParent(transform.FindChild("Canvas").transform);
+            initiationText.transform.localPosition = new Vector3(tokenObject.initiation1X, tokenObject.initiation1Y, -1.05f);
+            initiationText.transform.localRotation = new Quaternion(0, 0, 0, 0);
+            initiationText.transform.localScale = new Vector3(1, 1, 1);
+
+            if (tokenObject.doubleAttack)
+            {
+                GameObject initiationText2 = Instantiate(Resources.Load<GameObject>("Prefabs/InitiationText"));
+                initiationText2.transform.SetParent(transform.FindChild("Canvas").transform);
+                initiationText2.transform.localPosition = new Vector3(tokenObject.initiation2X, tokenObject.initiation2Y, -1.05f);
+                initiationText2.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                initiationText2.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+        Material[] materials = GetComponent<MeshRenderer>().materials;
+        materials[1] = Resources.Load<Material>("Materials/" + tokenObject.name);
+        GetComponent<MeshRenderer>().materials = materials;
     }
 
     // Update is called once per frame
--- HEX/Assets/Scripts/TokenUIScript.cs
+++ HEX/Assets/Scripts/TokenUIScript.cs
@@ -6,6 +6,8 @@ public class TokenUIScript : MonoBehaviour
 {
     public GameObject token;
     public GameObject inputManager;
+
+    public string name;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,7 @@ public class TokenUIScript : MonoBehaviour
     public void Clicked()
     {
         GameObject spawned = Instantiate(token, new Vector3(0,1,0), Quaternion.Euler(new Vector3(-90,0,0)));
+        spawned.GetComponent<TokenScript>().tokenObject = Resources.Load<TokenScriptableObject>("Tokens/" + name);
         inputManager.GetComponent<InputManagerScript>().SelectedToken = spawned;
         transform.parent.gameObject.SetActive(false);
     }
